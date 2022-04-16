ba3bffd4f35807be68e66aff5adbce39e424e38f
mrelectronst <fhkaraman@outlook.com>
2022-04-16 23:20:19 +0300

    Add project files.

---
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+
+        void Handle_Slider(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value : {0}", e.NewValue);
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NextPage());
+        }
+    }
+}
